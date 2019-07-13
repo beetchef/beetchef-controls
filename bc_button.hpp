e033f17a173be67ef90390d8cc0f9d3ce4f9c87a
obycajnanula <pkulcar@gmail.com>
2019-07-13 20:22:30 +0200

    Press types (#1)
    * press types added
    
    * printing events to serial port

--- bc_button.hpp
+++ bc_button.hpp
@@ -1,4 +1,6 @@
-
+#define SINGLE_PRESS "sp"
+#define DOUBLE_PRESS "dp"
+#define LONG_PRESS "lp"
 
 #include "Arduino.h"
 
@@ -6,13 +8,19 @@ class BcButton {
   public:
     BcButton(uint8_t pin);
     bool update(void);
-    bool getCurrentPinState();
+    String getEvent();
   private:
-    unsigned long mLastDebounceTime;
-    static const uint8_t debounceDelay = 10;
     uint8_t mPin;
+    uint8_t mPressCounter;
+    unsigned long mPressTimePoint;
+    unsigned long mReleaseTimePoint;
+    unsigned long mLastDebounceTime;
+    static const uint16_t mDoublePressGap = 500;
+    static const uint16_t mLongPressSpan = 500;
+    static const uint8_t mDebounceDelay = 10;
     bool mCurrentPinState;
     bool mLastPinState;
     bool mNewPressOccured;
-    void updateButtonState(bool updatedState);
+    void updateButtonState(bool updatedPinState);
+    String mEvent;
 };
